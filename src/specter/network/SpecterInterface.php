c8240a0226c642d6e485dfeb7a19adc14e9bb3d1
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-09-23 18:58:13 +0300

    Update SpecterInterface.php

--- src/specter/network/SpecterInterface.php
+++ src/specter/network/SpecterInterface.php
@@ -220,7 +220,7 @@ class SpecterInterface implements SourceInterface
                 $pk->clientData["SkinData"] = base64_encode(str_repeat("\x80", 64 * 32 * 4));
             }
             if (ProtocolInfo::CURRENT_PROTOCOL == 465){
-               $pk->clientData["SkinGeometryDataEngineVersion"] = base64_encode(str_repeat(random_bytes(3) ."\xff", 2048));
+               $pk->clientData["SkinGeometryDataEngineVersion"] = "Specter";
             }
             $pk->clientData["SkinImageHeight"] = 32;
             $pk->clientData["SkinImageWidth"] = 64;
